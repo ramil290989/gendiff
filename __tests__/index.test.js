329fcf376327bc444cccfba32b9585bc785f93bc
Ramil Garifullin <garifullinramil290989@gmail.com>
2022-11-29 00:33:10 +0500

    fix tests

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -1,7 +1,29 @@
 import genDiff from '../src/index.js';
 
-test('gendiff', () => {
+test('gendiff json-json', () => {
   expect(genDiff('./__fixtures__/file1.json', './__fixtures__/file2.json')).toBe(`{            
+  - follow: false
+    host: hexlet.io
+  - proxy: 123.234.53.22
+  - timeout: 50
+  + timeout: 20
+  + verbose: true
+}`);
+});
+
+test('gendiff yaml-yaml', () => {
+  expect(genDiff('./__fixtures__/file1.yaml', './__fixtures__/file2.yml')).toBe(`{            
+  - follow: false
+    host: hexlet.io
+  - proxy: 123.234.53.22
+  - timeout: 50
+  + timeout: 20
+  + verbose: true
+}`);
+});
+
+test('gendiff json-yaml', () => {
+  expect(genDiff('./__fixtures__/file1.json', './__fixtures__/file2.yml')).toBe(`{            
   - follow: false
     host: hexlet.io
   - proxy: 123.234.53.22
