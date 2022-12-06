daa872a2d7e91ba1fb85cf0ae33cbdcaca11d19f
Ramil Garifullin <garifullinramil290989@gmail.com>
2022-12-07 01:39:19 +0500

    src/formatters/stylish.js

--- src/formatters/stylish.js
+++ src/formatters/stylish.js
@@ -1,26 +1,38 @@
-const stylish = (diff) => {
-  const makeString = (diff, rep = 1) => {
+const stylish = (diffTree) => {
+  const makeString = (diffTree, rep = 1) => {
     rep += 1;
     let string = `{\n`;
-    let lines = Object.keys(diff).reduce((line, key) => {
-      if (diff[key] === null) {
-        diff[key] = 'null';
-      }
-      if (typeof diff[key] === 'object') {
-        rep += 1;
-        let subline = `${makeString(diff[key], rep)}`;
-        rep -= 1;
-        return line = `${line}${' '.repeat(rep)}${key}: ${subline}`;
-      } else {
-        line = `${line}${' '.repeat(rep)}${key}: ${diff[key]}\n`;
-        return line;
-      }
-    }, '');
-    string = `${string}${lines}${' '.repeat(rep - 2)}}\n`;
+		let lines = diffTree.reduce((line, item) => {
+			if (Array.isArray(item.value)) {
+				rep += 3;
+        item.value = `${makeString(item.value, rep)}`;
+				rep -= 3;
+			} 
+			switch (item.status) {
+				case 'nomod':
+					line = `${line}${' '.repeat(rep)}  ${item.key}: ${item.value}\n`;
+					break;
+				case 'changed_from':
+					line = `${line}${' '.repeat(rep)}- ${item.key}: ${item.value}\n`;
+					break;
+				case 'changed_to':
+					line = `${line}${' '.repeat(rep)}+ ${item.key}: ${item.value}\n`;
+					break;
+				case 'deleted':
+					line = `${line}${' '.repeat(rep)}- ${item.key}: ${item.value}\n`;
+					break;
+				case 'added':
+					line = `${line}${' '.repeat(rep)}+ ${item.key}: ${item.value}\n`;
+					break;
+				default:
+					throw new Error('что-то пошло не так');
+			}			
+			return line;
+		},'');
+		string = `${string}${lines}${' '.repeat(rep - 2)}}`;
     return string;
-  };
-  let result = makeString(diff);
-  return result.trim();
+	}
+	return makeString(diffTree);
 };
 
 export default stylish;
