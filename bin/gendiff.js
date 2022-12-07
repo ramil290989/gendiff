f527580a5f22de35fa7918b8c599f5c95ad70193
Ramil Garifullin <garifullinramil290989@gmail.com>
2022-12-08 00:22:19 +0500

    fix gendiff

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -4,7 +4,6 @@ import program from 'commander';
 import genDiff from '../src/index.js';
 
 program
-  .name('gendiff')
   .version('1.0.0', '-v, --version', 'output the current version')
   .arguments('<file1> <file2>')
   .description('Compares two configuration files and shows a difference.')
