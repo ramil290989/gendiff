0cf3aed342ef4c72c4eb3d85cc232c94c5302c55
Ramil Garifullin <garifullinramil290989@gmail.com>
2022-12-09 01:07:07 +0500

    fix gendifftree and getnodeswithoutmarkers

--- src/genDiffTree.js
+++ src/genDiffTree.js
@@ -1,4 +1,3 @@
-import getTreeKeys from './getTreeKeys.js';
 import getNodesWithMarkers from './getNodesWithMarkers.js';
 
 const genDiffTree = (file1Parse, file2Parse) => getNodesWithMarkers(file1Parse, file2Parse);
