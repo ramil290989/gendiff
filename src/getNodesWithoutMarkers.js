da1fff8b5fa9042015b75402cb9f92b714899d7f
Ramil Garifullin <garifullinramil290989@gmail.com>
2022-12-09 01:38:05 +0500

    fix getnodeswithoutmarkers

--- src/getNodesWithoutMarkers.js
+++ src/getNodesWithoutMarkers.js
@@ -5,15 +5,15 @@ const getNodesWithoutMarkers = (file1Parse, file2Parse) => {
   const nodesWithoutMarkers = keys.map((key) => {
     if (typeof file2Parse[key] !== 'object' && typeof file1Parse[key] !== 'object') {
       if (file2Parse === 0) {
-        return { key: key, value: file1Parse[key], status: 'nomod' };
+        return { key, value: file1Parse[key], status: 'nomod' };
       } else {
-        return { key: key, value: file2Parse[key], status: 'nomod' };
+        return { key, value: file2Parse[key], status: 'nomod' };
       }
     } else {
       if (file2Parse === 0) {
-        return { key: key, value: getNodesWithoutMarkers(file1Parse[key], 0), status: 'nomod' };
+        return { key, value: getNodesWithoutMarkers(file1Parse[key], 0), status: 'nomod' };
       } else {
-        return { key: key, value: getNodesWithoutMarkers(0, file2Parse[key]), status: 'nomod' };
+        return { key, value: getNodesWithoutMarkers(0, file2Parse[key]), status: 'nomod' };
       }
     }
   });
