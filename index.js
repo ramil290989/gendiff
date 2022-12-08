261d9cc83ee214c0b4f1812da9ff96390e785cac
Ramil Garifullin <garifullinramil290989@gmail.com>
2022-12-09 00:26:46 +0500

    fix index

--- index.js
+++ index.js
@@ -1,24 +1,23 @@
-import genDiffTree from './src/genDiffTree.js'
+import genDiffTree from './src/genDiffTree.js';
 import parser from './src/parser.js';
 import stylish from './src/formatters/stylish.js';
 import plain from './src/formatters/plain.js';
 import json from './src/formatters/json.js';
 
 const genDiff = (file1, file2, format = 'stylish') => {
-  let file1Parse = parser(file1);
-  let file2Parse = parser(file2);
-  let result = genDiffTree(file1Parse, file2Parse);
+  const file1Parse = parser(file1);
+  const file2Parse = parser(file2);
+  const result = genDiffTree(file1Parse, file2Parse);
   switch (format) {
     case 'stylish':
       return stylish(result);
-		case 'plain':
-			return plain(result);
-		case 'json':
-			return json(result);
-		default:
-			return stylish(result);
+    case 'plain':
+      return plain(result);
+    case 'json':
+      return json(result);
+    default:
+      return stylish(result);
   }
 };
 
 export default genDiff;
-
