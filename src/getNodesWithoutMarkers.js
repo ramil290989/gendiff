765bdcb565f71cfee22ac19b350cde916416ad8b
Ramil Garifullin <garifullinramil290989@gmail.com>
2022-12-09 12:17:12 +0500

    fix gendiffwithoutmarkers

--- src/getNodesWithoutMarkers.js
+++ src/getNodesWithoutMarkers.js
@@ -4,18 +4,13 @@ const getNodesWithoutMarkers = (file1Parse, file2Parse) => {
   const keys = getTreeKeys(file1Parse, file2Parse);
   const nodesWithoutMarkers = keys.map((key) => {
     if (typeof file2Parse[key] !== 'object' && typeof file1Parse[key] !== 'object') {
-      if (file2Parse === 0) {
-        return { key, value: file1Parse[key], status: 'nomod' };
-      } else {
-        return { key, value: file2Parse[key], status: 'nomod' };
-      }
-    } else {
-      if (file2Parse === 0) {
-        return { key, value: getNodesWithoutMarkers(file1Parse[key], 0), status: 'nomod' };
-      } else {
-        return { key, value: getNodesWithoutMarkers(0, file2Parse[key]), status: 'nomod' };
-      }
+      return file2Parse === 0 ?
+      { key, value: file1Parse[key], status: 'nomod' } :
+      { key, value: file2Parse[key], status: 'nomod' };
     }
+    return file2Parse === 0 ?
+    { key, value: getNodesWithoutMarkers(file1Parse[key], 0), status: 'nomod' } :
+    { key, value: getNodesWithoutMarkers(0, file2Parse[key]), status: 'nomod' };
   });
   return nodesWithoutMarkers;
 };
