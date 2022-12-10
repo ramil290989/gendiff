e7f9e5ac54bbd0d99f15998dafb74ca965f2b20b
Ramil Garifullin <garifullinramil290989@gmail.com>
2022-12-10 16:03:42 +0500

    fix getNodesWithMarkers

--- src/getNodesWithMarkers.js
+++ src/getNodesWithMarkers.js
@@ -9,51 +9,46 @@ const getNodesWithMarkers = (file1Parse, file2Parse) => {
         if (typeof file1Parse[key] !== 'object' && typeof file2Parse[key] !== 'object') {
           if (file1Parse[key] === file2Parse[key]) {
             return { key, value: file1Parse[key], status: 'nomod' };
-          } else {
-            return [
-              { key, value: file1Parse[key], status: 'changed_from' },
-              { key, value: file2Parse[key], status: 'changed_to' }
-            ];
           }
+          return [
+            { key, value: file1Parse[key], status: 'changed_from' },
+            { key, value: file2Parse[key], status: 'changed_to' }
+          ];
         } else if (typeof file1Parse[key] === 'object' && typeof file2Parse[key] !== 'object') {
           if (file1Parse[key] === null) {
             return [
               { key, value: file1Parse[key], status: 'changed_from' },
               { key, value: file2Parse[key], status: 'changed_to' }
             ];
-          } else {
-            return [
-              { key, value: getNodesWithoutMarkers(file1Parse[key], 0), status: 'changed_from' },
-              { key, value: file2Parse[key], status: 'changed_to' }
-            ];
           }
+          return [
+            { key, value: getNodesWithoutMarkers(file1Parse[key], 0), status: 'changed_from' },
+            { key, value: file2Parse[key], status: 'changed_to' }
+          ];
         } else if (typeof file1Parse[key] !== 'object' && typeof file2Parse[key] === 'object') {
           if (file2Parse[key] === null) {
             return [
               { key, value: file1Parse[key], status: 'changed_from' },
               { key, value: file2Parse[key], status: 'changed_to' }
             ];
-          } else {
-            return [
-              { key, value: file1Parse[key], status: 'changed_from' },
-              { key, value: getNodesWithoutMarkers(0, file2Parse[key]), status: 'changed_to' }
-            ];
           }
+          return [
+            { key, value: file1Parse[key], status: 'changed_from' },
+            { key, value: getNodesWithoutMarkers(0, file2Parse[key]), status: 'changed_to' }
+          ];
         } else {
           return { key, value: getNodesWithMarkers(file1Parse[key], file2Parse[key]), status: 'nomod' };
         }
-      } 
+      }
       if (typeof file1Parse[key] !== 'object') {
         return { key, value: file1Parse[key], status: 'deleted' };
-      } else {
-        return { key, value: getNodesWithoutMarkers(file1Parse[key], 0), status: 'deleted' };
       }
+      return { key, value: getNodesWithoutMarkers(file1Parse[key], 0), status: 'deleted' };
     }
     if (typeof file1Parse[key] !== 'object' && typeof file2Parse[key] !== 'object') {
       return { key, value: file2Parse[key], status: 'added' };
-    } else {
-      return { key, value: getNodesWithoutMarkers(0, file2Parse[key]), status: 'added' };
     }
+    return { key, value: getNodesWithoutMarkers(0, file2Parse[key]), status: 'added' };
   });
   return nodesWithMarkers.flat();
 };
