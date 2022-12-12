ce8e259871826cfb369b3090eba75f928456a76d
Ramil Garifullin <garifullinramil290989@gmail.com>
2022-12-13 03:03:25 +0500

    fix linter

--- src/getNodesWithoutMarkers.js
+++ src/getNodesWithoutMarkers.js
@@ -2,11 +2,12 @@ import _ from 'lodash';
 
 const getNodesWithoutMarkers = (fileParse) => {
   const keys = _.sortBy(Object.keys(fileParse));
-  return keys.map((key) =>
-    _.isObject(fileParse[key])
-      ? { key, value: getNodesWithoutMarkers(fileParse[key]), status: 'nomod' }
-      : { key, value: fileParse[key], status: 'nomod' }
-  );
+  return keys.map((key) => {
+    if (_.isObject(fileParse[key])) {
+      return { key, value: getNodesWithoutMarkers(fileParse[key]), status: 'nomod' };
+    }
+    return { key, value: fileParse[key], status: 'nomod' };
+  });
 };
 
 export default getNodesWithoutMarkers;
