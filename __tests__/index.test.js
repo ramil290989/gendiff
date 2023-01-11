774b21120f90e9c87d927388ae0831158216ed36
Ramil Garifullin <garifullinramil290989@gmail.com>
2023-01-12 04:12:21 +0500

    fix

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -1,22 +1,46 @@
 import fs from 'fs';
 import genDiff from '../index.js';
 
-test('format stylish fileType json vs json', () => {
-  expect(genDiff('./__fixtures__/file1-hex.json', './__fixtures__/file2-hex.json')).toBe(fs.readFileSync('./__fixtures__/result-stylish', 'utf-8').trim());
+const file1JSON = './__fixtures__/file1-hex.json';
+const file2JSON = './__fixtures__/file2-hex.json';
+const file1YAML = './__fixtures__/file1-hex.yml';
+const file2YAML = './__fixtures__/file2-hex.yml';
+const stylish = fs.readFileSync('./__fixtures__/result-stylish', 'utf-8').trim();
+const plain = fs.readFileSync('./__fixtures__/result-plain', 'utf-8').trim();
+const json = fs.readFileSync('./__fixtures__/result-json', 'utf-8').trim();
+
+const toTest1 = [
+  [file1JSON, file2JSON, 'stylish'],
+  [file1JSON, file2YAML, 'stylish'],
+  [file1YAML, file2YAML, 'stylish'],
+];
+
+const toTest2 = [
+  [file1JSON, file2JSON, 'plain'],
+  [file1JSON, file2YAML, 'plain'],
+  [file1YAML, file2YAML, 'plain'],
+];
+
+const toTest3 = [
+  [file1JSON, file2JSON, 'json'],
+  [file1JSON, file2YAML, 'json'],
+  [file1YAML, file2YAML, 'json'],
+];
+
+describe('Test stylis', () => {
+  test.each(toTest1)(`stylish`, (...params) => {
+    expect(genDiff(...params)).toEqual(stylish);
+  });
 });
 
-test('format plain fileType json vs json', () => {
-  expect(genDiff('./__fixtures__/file1-hex.json', './__fixtures__/file2-hex.json', 'plain')).toBe(fs.readFileSync('./__fixtures__/result-plain', 'utf-8').trim());
+describe('Test plain', () => {
+  test.each(toTest2)(`plain`, (...params) => {
+    expect(genDiff(...params)).toEqual(plain);
+  });
 });
 
-test('format json fileType json vs json', () => {
-  expect(genDiff('./__fixtures__/file1-hex.json', './__fixtures__/file2-hex.json', 'json')).toBe(fs.readFileSync('./__fixtures__/result-json', 'utf-8').trim());
-});
-
-test('format json fileType yaml vs json', () => {
-  expect(genDiff('./__fixtures__/file1-hex.yml', './__fixtures__/file2-hex.json', 'json')).toBe(fs.readFileSync('./__fixtures__/result-json', 'utf-8').trim());
-});
-
-test('format json fileType yaml vs yaml', () => {
-  expect(genDiff('./__fixtures__/file1-hex.yml', './__fixtures__/file2-hex.yml', 'json')).toBe(fs.readFileSync('./__fixtures__/result-json', 'utf-8').trim());
+describe('Test json', () => {
+  test.each(toTest3)(`json`, (...params) => {
+    expect(genDiff(...params)).toEqual(json);
+  });
 });
--- src/formatters/stylish.js
+++ src/formatters/stylish.js
@@ -1,39 +1,75 @@
 import _ from 'lodash';
 
-const valueToString = (itemValue, repeatValue) => {
-  const repeatBeforeKey = repeatValue + 2;
-  const repeatForNested = repeatValue + 4;
-  const repeatAfterValue = repeatValue - 2;
+const spacesToNested = 4;
+
+const getSpaces = (spaceCount, to) => {
+  const beforeKey = 0;
+  const beforeNestedKey = 2;
+  const afterValue = -2;
+  switch (to) {
+    case 'beforeKey':
+      return ' '.repeat(spaceCount + beforeKey);
+    case 'beforeNestedKey':
+      return ' '.repeat(spaceCount + beforeNestedKey);
+    case 'afterValue':
+      return ' '.repeat(spaceCount + afterValue);
+    default:
+      throw new Error('неверное значение для функции');
+  };
+}
+
+const getOperator = (status) => {
+  switch (status) {
+    case 'nomod':
+      return ' ';
+    case 'added':
+      return '+';
+    case 'deleted':
+      return '-';
+    default:
+      throw new Error('неверный статус');
+  }
+};
+
+const valueToString = (itemValue, spaceCount) => {
+  const nestedSpaceCount = spaceCount + spacesToNested;
   if (_.isObject(itemValue)) {
-    const nestedLine = Object.keys(itemValue).map((key) => `${' '.repeat(repeatBeforeKey)}${key}: ${valueToString(itemValue[key], repeatForNested)}`)
+    const keys = Object.keys(itemValue);
+    const nestedLine = keys.map((key) => {
+      const value = valueToString(itemValue[key], nestedSpaceCount);
+      return `${getSpaces(spaceCount, 'beforeNestedKey')}${key}: ${value}`;
+    })
       .join('\n');
-    return `{\n${nestedLine}\n${' '.repeat(repeatAfterValue)}}`;
+    return `{\n${nestedLine}\n${getSpaces(spaceCount, 'afterValue')}}`;
   }
   return itemValue;
 };
 
-const makeString = (diffTree, repeatValue = 2) => {
-  const repeatBeforeKey = repeatValue;
-  const repeatForNested = repeatValue + 4;
-  const repeatAfterValue = repeatValue - 2;
+const makeString = (diffTree, spaceCount = 2) => {
   const lines = diffTree.map((item) => {
+    const spiceBeforeKey = getSpaces(spaceCount, 'beforeKey');
+    const key = item.key;
+    const nestedSpaceCount = spaceCount + spacesToNested;
     switch (item.status) {
       case 'nomod':
-        return `${' '.repeat(repeatBeforeKey)}  ${item.key}: ${valueToString(item.value, repeatForNested)}`;
-      case 'changed':
-        return `${' '.repeat(repeatBeforeKey)}- ${item.key}: ${valueToString(item.oldValue, repeatForNested)}\n${' '.repeat(repeatBeforeKey)}+ ${item.key}: ${valueToString(item.newValue, repeatForNested)}`;
-      case 'deleted':
-        return `${' '.repeat(repeatBeforeKey)}- ${item.key}: ${valueToString(item.value, repeatForNested)}`;
       case 'added':
-        return `${' '.repeat(repeatBeforeKey)}+ ${item.key}: ${valueToString(item.value, repeatForNested)}`;
+      case 'deleted':
+        const operator = getOperator(item.status);
+        const value = valueToString(item.value, nestedSpaceCount);
+        return `${spiceBeforeKey}${operator} ${key}: ${value}`;
       case 'nested':
-        return `${' '.repeat(repeatBeforeKey)}  ${item.key}: ${makeString(item.value, repeatForNested)}`;
+        const nestedValue = makeString(item.value, nestedSpaceCount)
+        return `${spiceBeforeKey}  ${key}: ${nestedValue}`;
+      case 'changed':
+        const oldValue = valueToString(item.oldValue, nestedSpaceCount);
+        const newValue = valueToString(item.newValue, nestedSpaceCount);
+        return `${spiceBeforeKey}- ${key}: ${oldValue}\n${spiceBeforeKey}+ ${key}: ${newValue}`;
       default:
         throw new Error('неверный статус');
     }
   })
     .join('\n');
-  return `{\n${lines}\n${' '.repeat(repeatAfterValue)}}`;
+  return `{\n${lines}\n${getSpaces(spaceCount, 'afterValue')}}`;
 };
 
 const stylish = (diffTree) => makeString(diffTree);
